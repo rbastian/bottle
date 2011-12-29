68aa079104e18565e013c921a94afc4a38d6ab7c
Rob Bastian <rob.bastian@tippr.com>
2011-12-29 14:06:14 -0600

    Adding a "template" to the code

--- todo.py
+++ todo.py
@@ -5,11 +5,37 @@ import bottle
 
 @bottle.route('/todo')
 def todo_list():
+    """
+
+    """
     conn = sqlite3.connect('todo.db')
     c = conn.cursor()
     c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
     result = c.fetchall()
     output = bottle.template('make_table', rows=result)
     return output
+
+@bottle.route('/new', method='GET')
+def new_item():
+
+    """
+
+    """
+
+    if bottle.request.GET.get('save','').strip():
+
+        new = bottle.request.GET.get('task', '').strip()
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+
+        c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new,1))
+        new_id = c.lastrowid
+
+        conn.commit()
+        c.close()
+
+        return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
+    else:
+        return bottle.template('new_task.tpl')
 
 bottle.run()
